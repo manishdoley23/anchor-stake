45525dcb3968e92c2e7e7d13607566e1033adabb
manishdoley23 <manishdoley23@gmail.com>
2024-04-29 22:33:07 +0530

    update

--- programs/staking-program/src/lib.rs
+++ programs/staking-program/src/lib.rs
@@ -1,6 +1,13 @@
-use anchor_lang::prelude::*;
+use anchor_lang::{prelude::*, Accounts};
+use anchor_spl::token::{self, Mint, Token, TokenAccount};
 
 declare_id!("GpJ45EtdnvBxETx3kX5afhjq1Qs7tmnRExp8Zf734mK7");
+
+pub mod constants {
+    pub const VAULT_SEED: &[u8] = b"vault";
+    pub const STAKE_INFO_SEED: &[u8] = b"stake_info";
+    pub const TOKEN_SEED: &[u8] = b"token";
+}
 
 #[program]
 pub mod staking_program {
@@ -9,7 +16,31 @@ pub mod staking_program {
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
         Ok(())
     }
+
+    pub fn stake(ctx: Context<Initialize>, amount: u64) -> Result<()> {
+        Ok(())
+    }
+
+    pub fn destake(ctx: Context<Initialize>) -> Result<()> {
+        Ok(())
+    }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Initialize<'info> {
+    #[account(mut)]
+    pub signer: Signer<'info>,
+
+    #[account(
+        init_if_needed,
+        seeds = [constants::VAULT_SEED]
+        bump,
+        payer = signer,
+        token::mint = mint,
+        token::authority = token_vault_account,
+    )]
+    pub token_vault_account: Account<'info, TokenAccount>,
+    pub mint: Account<'info, Mint>,
+    pub token_program: Program<'info, Token>,
+    pub system_program: Program<'info, System>,
+}
